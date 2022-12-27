0dc6b61b7c3bbabecdd389d60c2d51bbd63c7743
zackhamin <ishaqhamin@gmail.com>
2022-12-27 12:08:55 +0000

    chore(validator): middleware validator for routes

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { body, validationResult } from "express-validator";
+import { body, oneOf, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 
@@ -9,30 +10,55 @@ router.get("/product", (req, res) => {
   res.json({ message: "You are the chosen one" });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", body("name").isString(), (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400);
-    res.json({ errors: errors.array() });
-  }
-});
-router.post("/product", () => {});
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
+router.post("/product", body("name").isString(), handleInputErrors, () => {});
 router.delete("/product/:id", () => {});
 
 //Update
 
 router.get("/update", () => {});
-router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
-router.post("/update", () => {});
+router.get("/update/:id", body("id").isUUID(), () => {});
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+  handleInputErrors,
+  () => {}
+);
+router.post(
+  "/update",
+  body("title").exists(),
+  body("body").exists(),
+  handleInputErrors,
+  () => {}
+);
 router.delete("/update/:id", () => {});
 
-//Update
+//Update points
 
 router.get("/updateinfo", () => {});
-router.get("/updateinfo/:id", () => {});
-router.put("/updateinfo/:id", () => {});
-router.post("/updateinfo", () => {});
+router.get("/updateinfo/:id", body("id").isUUID(), () => {});
+router.put(
+  "/updateinfo/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  handleInputErrors,
+  () => {}
+);
+router.post(
+  "/updateinfo",
+  body("name").exists().isString(),
+  body("description").exists().isString(),
+  body("UpdateInfoId").exists().isString(),
+  handleInputErrors,
+  () => {}
+);
 router.delete("/updateinfo/:id", () => {});
 
 export { router };
